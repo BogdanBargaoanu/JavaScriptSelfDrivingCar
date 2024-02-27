697ee9c3766f46fbd23283dfae13459ebb8268c3
BogdanBargaoanu <bn19bba@yahoo.com>
2024-02-27 23:59:38 +0200

    Implemented sensors

--- car.js
+++ car.js
@@ -9,11 +9,13 @@ class Car{
         this.maxSpeed = 3;
         this.friction = 0.05;
         this.angle = 0;
+        this.sensor = new Sensor(this);
         this.controls = new Controls();
     }
 
-update() {
+update(roadBorders) {
     this.#move();
+    this.sensor.update(roadBorders);
 }
 
 #move() {
@@ -58,5 +60,6 @@ update() {
         ctx.fillStyle = "red";
         ctx.fillRect(-this.width/2,-this.height/2,this.width,this.height);
         ctx.restore();
+        this.sensor.draw(ctx);
     }
 }
